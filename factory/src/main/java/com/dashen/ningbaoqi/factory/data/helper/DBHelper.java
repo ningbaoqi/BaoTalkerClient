25e85cb0d83f4583ce72b72ac3645685809b7b9f
ningbaoqi <baoqi.ning@new-bund.net>
2018-07-12 14:33:20 +0800

    添加数据库保存更新观察者

--- factory/src/main/java/com/dashen/ningbaoqi/factory/data/helper/DBHelper.java
+++ factory/src/main/java/com/dashen/ningbaoqi/factory/data/helper/DBHelper.java
@@ -1,6 +1,8 @@
 package com.dashen.ningbaoqi.factory.data.helper;
 
 import com.dashen.ningbaoqi.factory.model.db.AppDatabase;
+import com.dashen.ningbaoqi.factory.model.db.GroupMember;
+import com.dashen.ningbaoqi.factory.model.db.Message;
 import com.raizlabs.android.dbflow.config.DatabaseDefinition;
 import com.raizlabs.android.dbflow.config.FlowManager;
 import com.raizlabs.android.dbflow.structure.BaseModel;
@@ -9,6 +11,10 @@ import com.raizlabs.android.dbflow.structure.database.DatabaseWrapper;
 import com.raizlabs.android.dbflow.structure.database.transaction.ITransaction;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * 数据库的辅助工具类：辅助完成增删改
@@ -16,12 +22,58 @@ import java.util.Arrays;
 public class DBHelper {
     private static final DBHelper instance;
 
-
     private DBHelper() {
     }
 
     static {
         instance = new DBHelper();
+    }
+
+    private final Map<Class<?>, Set<ChangedListener>> changedListeners = new HashMap<>();//观察者集合：观察的表：每一个表的观察者有很多
+
+    /**
+     * 从所有的监听器中，获取某一个表的所有监听者
+     *
+     * @param modelClass 表对应的Class信息
+     * @param <Model>    泛型
+     * @return Set<ChangedListener>
+     */
+    private <Model extends BaseModel> Set<ChangedListener> getListeners(Class<Model> modelClass) {
+        if (changedListeners.containsKey(modelClass)) {
+            return changedListeners.get(modelClass);
+        }
+        return null;
+    }
+
+    /**
+     * 添加一个监听
+     *
+     * @param tClass   对某个表关注
+     * @param listener 监听者
+     * @param <Model>  表的泛型
+     */
+    public static <Model extends BaseModel> void addChangedListener(final Class<Model> tClass, ChangedListener<Model> listener) {
+        Set<ChangedListener> changedListeners = instance.getListeners(tClass);
+        if (changedListeners == null) {
+            changedListeners = new HashSet<>();//初始化某一类型的容器
+            instance.changedListeners.put(tClass, changedListeners);//添加到总的Map
+        }
+        changedListeners.add(listener);
+    }
+
+    /**
+     * 删除指定表的监听器
+     *
+     * @param tClass   表的Class信息
+     * @param listener 监听器
+     * @param <Model>  表的泛型
+     */
+    public static <Model extends BaseModel> void removeChangedListener(final Class<Model> tClass, ChangedListener<Model> listener) {
+        Set<ChangedListener> changedListeners = instance.getListeners(tClass);
+        if (changedListeners == null) {
+            return;
+        }
+        changedListeners.remove(listener);
     }
 
     /**
@@ -76,8 +128,38 @@ public class DBHelper {
      * @param <Model> 数据类型
      */
     private final <Model extends BaseModel> void notifySave(final Class<Model> tClass, final Model... models) {
-        // TODO
+        final Set<ChangedListener> listeners = getListeners(tClass);
+        if (listeners != null && listeners.size() > 0) {
+            for (ChangedListener<Model> listener : listeners) {//通用的通知
+                listener.onDataSave(models);
+            }
+        }
+
+        if (GroupMember.class.equals(tClass)) { //例外情况：群成员变更需要通知对应的群信息更新
+            updateGroup((GroupMember[]) models);
+        } else if (Message.class.equals(tClass)) {//例外情况：消息变化应该通知会话列表更新
+            updateSession((Message[]) models);
+        }
     }
+
+    /**
+     * 从成员中找出成员对应的群，并对群进行更新
+     *
+     * @param members 群成员列表
+     */
+    private void updateGroup(GroupMember... members) {
+
+    }
+
+    /**
+     * 从消息列表中筛选中对应的回话，并对会话进行更新
+     *
+     * @param messages
+     */
+    private void updateSession(Message... messages) {
+
+    }
+
 
     /**
      * 通知删除
@@ -88,5 +170,16 @@ public class DBHelper {
      */
     private final <Model extends BaseModel> void notifyDelete(final Class<Model> tClass, final Model... models) {
         // TODO
+    }
+
+    /**
+     * 通知监听器
+     */
+    public interface ChangedListener<Data> {
+
+        void onDataSave(Data... datas);
+
+        void onDataDelete(Data... datas);
+
     }
 }
