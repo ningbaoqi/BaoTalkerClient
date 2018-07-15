8cfbad60ea2461f09888c74d2538f72c0dfbe385
ningbaoqi <1031297081@qq.com>
2018-07-15 11:56:16 +0800

    聊天界面布局

--- app/src/main/java/project/com/ningbaoqi/baotalkerclient/fragment/message/ChatFragment.java
+++ app/src/main/java/project/com/ningbaoqi/baotalkerclient/fragment/message/ChatFragment.java
@@ -1,12 +1,25 @@
 package project.com.ningbaoqi.baotalkerclient.fragment.message;
 
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
+import butterknife.BindView;
+import project.com.ningbaoqi.baotalkerclient.R;
 import project.com.ningbaoqi.baotalkerclient.activities.MessageActivity;
 import project.com.ningbaoqi.common.app.Fragment;
 
-public abstract class ChatFragment extends Fragment {
+public abstract class ChatFragment extends Fragment implements AppBarLayout.OnOffsetChangedListener {
     private String mReceiverId;
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+    @BindView(R.id.recycler)
+    RecyclerView mRecycler;
+    @BindView(R.id.appbar)
+    AppBarLayout mAppBarLayout;
 
     @Override
     protected void initArgs(Bundle bundle) {
@@ -14,5 +27,34 @@ public abstract class ChatFragment extends Fragment {
         mReceiverId = bundle.getString(MessageActivity.KEY_RECEIVER_ID);
     }
 
-    
+    @Override
+    protected void initWidget(View root) {
+        super.initWidget(root);
+        initToolBar();
+        initAppBar();
+        mRecycler.setLayoutManager(new LinearLayoutManager(getContext()));
+    }
+
+    private void initToolBar() {
+        Toolbar toolbar = mToolbar;
+        toolbar.setNavigationIcon(R.drawable.ic_back);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().finish();
+            }
+        });
+    }
+
+    /**
+     * //给界面的AppBar设置一个监听，得到关闭与打开的时候的进度
+     */
+    private void initAppBar() {
+        mAppBarLayout.addOnOffsetChangedListener(this);
+    }
+
+    @Override
+    public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+        
+    }
 }
--- app/src/main/java/project/com/ningbaoqi/baotalkerclient/fragment/message/ChatUserFragment.java
+++ app/src/main/java/project/com/ningbaoqi/baotalkerclient/fragment/message/ChatUserFragment.java
@@ -1,10 +1,48 @@
 package project.com.ningbaoqi.baotalkerclient.fragment.message;
 
-import project.com.ningbaoqi.baotalkerclient.R;
+import android.support.design.widget.AppBarLayout;
+import android.view.View;
 
-public class ChatUserFragment extends ChatFragment{
+import butterknife.BindView;
+import project.com.ningbaoqi.baotalkerclient.R;
+import project.com.ningbaoqi.common.widget.a.PortraitView;
+
+/**
+ * 用户聊天界面
+ */
+public class ChatUserFragment extends ChatFragment {
+    @BindView(R.id.im_portrait)
+    PortraitView mPortraitView;
+
     @Override
     protected int getContentLayoutId() {
         return R.layout.fragment_chat_user;
+    }
+
+    @Override
+    public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+        super.onOffsetChanged(appBarLayout, verticalOffset);
+        View view = mPortraitView;
+        if (verticalOffset == 0) {//完全展开
+            view.setVisibility(View.VISIBLE);
+            view.setScaleX(1);
+            view.setScaleY(1);
+            view.setAlpha(1);
+        } else {
+            verticalOffset = Math.abs(verticalOffset);
+            final int totalScrollRange = appBarLayout.getTotalScrollRange();
+            if (verticalOffset >= totalScrollRange) {//关闭状态
+                view.setVisibility(View.INVISIBLE);
+                view.setScaleX(0);
+                view.setScaleY(0);
+                view.setAlpha(0);
+            } else {//中间状态
+                float process = 1 - verticalOffset / totalScrollRange;
+                view.setVisibility(View.VISIBLE);
+                view.setScaleX(process);
+                view.setScaleY(process);
+                view.setAlpha(process);
+            }
+        }
     }
 }
