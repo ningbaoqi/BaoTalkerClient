cc55d0880cb88b93b9bfa214d9f21314a302b46c
ningbaoqi <1031297081@qq.com>
2018-07-16 23:36:08 +0800

    Fix bug ToolBar滑动隐藏

--- app/src/main/java/project/com/ningbaoqi/baotalkerclient/fragment/message/ChatGroupFragment.java
+++ app/src/main/java/project/com/ningbaoqi/baotalkerclient/fragment/message/ChatGroupFragment.java
@@ -1,5 +1,7 @@
 package project.com.ningbaoqi.baotalkerclient.fragment.message;
 
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
@@ -9,6 +11,9 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.ViewTarget;
 import com.dashen.ningbaoqi.factory.model.db.Group;
 import com.dashen.ningbaoqi.factory.model.db.view.MemberUserModel;
 import com.dashen.ningbaoqi.factory.presenter.message.ChatContract;
@@ -34,6 +39,53 @@ public class ChatGroupFragment extends ChatFragment<Group> implements ChatContra
     @Override
     protected int getHeaderLayoutId() {
         return R.layout.lay_chat_header_group;
+    }
+
+    @Override
+    protected void initWidget(View root) {
+        super.initWidget(root);
+        Glide.with(this).load(R.mipmap.default_banner_group).centerCrop().into(new ViewTarget<CollapsingToolbarLayout, GlideDrawable>(mCollapsingToolbarLayout) {
+            @Override
+            public void onResourceReady(GlideDrawable resource, GlideAnimation<? super GlideDrawable> glideAnimation) {
+                this.view.setContentScrim(resource.getCurrent());
+            }
+        });
+    }
+
+    /**
+     * 进行高度的综合运算，透明我们的头像和Icon
+     *
+     * @param appBarLayout
+     * @param verticalOffset
+     */
+    @Override
+    public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+        super.onOffsetChanged(appBarLayout, verticalOffset);
+        View view = mLayMembers;
+        if (view == null) {
+            return;
+        }
+        if (verticalOffset == 0) {//完全展开
+            view.setVisibility(View.VISIBLE);
+            view.setScaleX(1);
+            view.setScaleY(1);
+            view.setAlpha(1);
+        } else {
+            verticalOffset = Math.abs(verticalOffset);
+            final int totalScrollRange = appBarLayout.getTotalScrollRange();
+            if (verticalOffset >= totalScrollRange) {//关闭状态
+                view.setVisibility(View.INVISIBLE);
+                view.setScaleX(0);
+                view.setScaleY(0);
+                view.setAlpha(0);
+            } else {//中间状态
+                float process = 1 - verticalOffset / totalScrollRange;
+                view.setVisibility(View.VISIBLE);
+                view.setScaleX(process);
+                view.setScaleY(process);
+                view.setAlpha(process);
+            }
+        }
     }
 
     @Override
