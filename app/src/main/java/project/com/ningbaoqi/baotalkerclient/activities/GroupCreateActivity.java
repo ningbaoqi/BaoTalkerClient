6501f1dca55eb6c86209210bb0f6a2a873f8eef3
ningbaoqi <baoqi.ning@new-bund.net>
2018-07-16 14:02:08 +0800

    资源导入

---
+++ app/src/main/java/project/com/ningbaoqi/baotalkerclient/activities/GroupCreateActivity.java
@@ -0,0 +1,11 @@
+package project.com.ningbaoqi.baotalkerclient.activities;
+
+import project.com.ningbaoqi.baotalkerclient.R;
+import project.com.ningbaoqi.common.app.ToolbarActivity;
+
+public class GroupCreateActivity extends ToolbarActivity {
+    @Override
+    protected int getContentLayoutId() {
+        return R.layout.activity_group_create;
+    }
+}
