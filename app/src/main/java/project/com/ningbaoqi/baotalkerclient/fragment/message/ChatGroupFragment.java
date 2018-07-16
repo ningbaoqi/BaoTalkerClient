8688961cdc4cbb75b45a9fd3a5244ead75b53cb4
ningbaoqi <1031297081@qq.com>
2018-07-16 21:48:51 +0800

    UI逻辑

--- app/src/main/java/project/com/ningbaoqi/baotalkerclient/fragment/message/ChatGroupFragment.java
+++ app/src/main/java/project/com/ningbaoqi/baotalkerclient/fragment/message/ChatGroupFragment.java
@@ -1,8 +1,12 @@
 package project.com.ningbaoqi.baotalkerclient.fragment.message;
 
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.dashen.ningbaoqi.factory.model.db.Group;
@@ -14,6 +18,7 @@ import java.util.List;
 
 import butterknife.BindView;
 import project.com.ningbaoqi.baotalkerclient.R;
+import project.com.ningbaoqi.baotalkerclient.activities.PersonalActivity;
 
 /**
  * 群聊天界面
@@ -21,6 +26,10 @@ import project.com.ningbaoqi.baotalkerclient.R;
 public class ChatGroupFragment extends ChatFragment<Group> implements ChatContract.GroupView {
     @BindView(R.id.im_header)
     ImageView mHeader;
+    @BindView(R.id.lay_members)
+    LinearLayout mLayMembers;
+    @BindView(R.id.txt_member_more)
+    TextView mMemberMore;
 
     @Override
     protected int getHeaderLayoutId() {
@@ -40,7 +49,32 @@ public class ChatGroupFragment extends ChatFragment<Group> implements ChatContra
 
     @Override
     public void onInitGroupMembers(List<MemberUserModel> members, long moreCount) {
-
+        if (members == null || members.size() == 0) {
+            return;
+        }
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        for (final MemberUserModel member : members) {
+            ImageView p = (ImageView) inflater.inflate(R.layout.lay_chat_group_potrait, mLayMembers, false);
+            mLayMembers.addView(p, 0);//添加成员头像
+            Glide.with(this).load(member.portrait).placeholder(R.drawable.default_portrait).centerCrop().dontAnimate().into(p);
+            p.setOnClickListener(new View.OnClickListener() {//个人界面信息查看
+                @Override
+                public void onClick(View v) {
+                    PersonalActivity.show(getContext(), member.userId);
+                }
+            });
+        }
+        if (moreCount > 0) {//更多的按钮
+            mMemberMore.setText(String.format("+%s", moreCount));
+            mMemberMore.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // TODO 显示成员列表
+                }
+            });
+        } else {
+            mMemberMore.setVisibility(View.GONE);
+        }
     }
 
     @Override
